022ccb2232d6bcfbb1d79e854e5dfa9564199bf8
Luis Gabriel <barros.luissalv@gmail.com>
2024-04-21 19:49:36 -0300

    Getting post by ID

---
+++ src/main/java/com/springboot/mongoDB/service/PostService.java
@@ -0,0 +1,23 @@
+package com.springboot.mongoDB.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.mongoDB.domain.entities.Post;
+import com.springboot.mongoDB.repository.PostRepository;
+import com.springboot.mongoDB.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+}
+
