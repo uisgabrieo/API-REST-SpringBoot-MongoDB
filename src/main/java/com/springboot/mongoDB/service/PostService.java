d969a0381a6f00be069315f4eaed17d9fe7985b0
Luis Gabriel <barros.luissalv@gmail.com>
2024-04-22 13:43:36 -0300

    Adjustments to the query

--- src/main/java/com/springboot/mongoDB/service/PostService.java
+++ src/main/java/com/springboot/mongoDB/service/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return postRepository.findByTitleContaining(text);
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
 
